cd32cb40d42c63cbbd18e219507d403f10502a08
arifinfrds <arifinfrds.engineer@gmail.com>
2022-12-13 14:17:14 +0700

    `findDuplicateCharacter` delivers empty dictionary on empty string

---
+++ FindDuplicateCharacterInStringTests/ProgramTests.swift
@@ -0,0 +1,25 @@
+//
+//  ProgramTests.swift
+//  FindDuplicateCharacterInStringTests
+//
+//  Created by Arifin Firdaus on 13/12/22.
+//
+
+import XCTest
+
+struct Program {
+    func findDuplicateCharacter(in string: String) -> [Character: Int] {
+        return [:]
+    }
+}
+
+final class ProgramTests: XCTestCase {
+
+    func test_findDuplicateCharacter_deliversEmptyOnEmptyString() {
+        let sut = Program()
+        
+        let result = sut.findDuplicateCharacter(in: "")
+        
+        XCTAssertEqual(result, [:])
+    }
+}
